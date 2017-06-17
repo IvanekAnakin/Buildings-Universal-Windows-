a3594a66b070e2391b8c20f19b9d8e911247d983
Ivan Anikin <IvanekAnakin@users.noreply.github.com>
2017-06-17 21:56:34 +0200

    Just renamed to "Buildings"

--- Buildings(Universal Windows)/MainPage.xaml.cs
+++ Buildings(Universal Windows)/MainPage.xaml.cs
@@ -13,18 +13,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Database_2_Universal_Windows_
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage2 : Page
+    public sealed partial class MainPage : Page
     {
-        public BlankPage2()
+        public MainPage()
         {
             this.InitializeComponent();
+            async void Continue(object sender, EventArgs args)
+            {
+                this.Frame.Navigate(typeof(), null);
+            }
         }
     }
 }
